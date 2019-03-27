7c22c3a25757edae5819c192c9645eac08a6c215
Knwright <43178849+knwright@users.noreply.github.com>
2019-03-26 21:14:18 -0500

    line_graph_working

--- line_graph.js
+++ line_graph.js
@@ -1,7 +1,30 @@
 
 // Retrieve CSV data
-d3.csv("data/bfro_reports_geocoded.csv", function(data) {
+d3.csv("bfro_reports_geocoded.csv", function(data) {
     console.log(data);
   });
 
 // Build line plot with traces
+var trace1 = {
+    x: data.map(row => row.date.year),
+    y: data.map(row => row.),
+    mode = 'lines+markers'
+};
+var trace2 = {
+    x: data.map(row => row.date.year),
+    y: data.map(row => row.),
+    mode = 'lines+markers'
+};
+var trace3 = {
+    x: data.map(row => row.date.year),
+    y: data.map(row => row.),
+    mode = 'lines+markers'
+};
+var graph_data = [trace1];
+
+var layout = {
+    title: "Sasquatch Sightings by Year",
+    xaxis: { title: "Year" },
+    yaxis: { title: "Number of Sightings"}
+}
+Plotly.newPlot("plot", data, layout);
