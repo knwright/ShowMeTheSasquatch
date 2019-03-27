baf76e16e4c309409b6d553d786a9c01ed8ca425
Knwright <43178849+knwright@users.noreply.github.com>
2019-03-26 21:43:39 -0500

    updated json

--- line_graph.js
+++ line_graph.js
@@ -1,30 +1,30 @@
 
-// Retrieve CSV data
-d3.csv("bfro_reports_geocoded.csv", function(data) {
-    console.log(data);
-  });
+// Retrieve JSON data
+d3.json("bfro_reports.json").then(function(bf_data) {
+    console.log(bf_data);
+});
 
 // Build line plot with traces
-var trace1 = {
-    x: data.map(row => row.date.year),
-    y: data.map(row => row.),
-    mode = 'lines+markers'
-};
-var trace2 = {
-    x: data.map(row => row.date.year),
-    y: data.map(row => row.),
-    mode = 'lines+markers'
-};
-var trace3 = {
-    x: data.map(row => row.date.year),
-    y: data.map(row => row.),
-    mode = 'lines+markers'
-};
-var graph_data = [trace1];
+// var trace1 = {
+//     x: data.map(row => row.date.year),
+//     y: data.map(row => row.),
+//     mode = 'lines+markers'
+// };
+// var trace2 = {
+//     x: data.map(row => row.date.year),
+//     y: data.map(row => row.),
+//     mode = 'lines+markers'
+// };
+// var trace3 = {
+//     x: data.map(row => row.date.year),
+//     y: data.map(row => row.),
+//     mode = 'lines+markers'
+// };
+// var graph_data = [trace1];
 
-var layout = {
-    title: "Sasquatch Sightings by Year",
-    xaxis: { title: "Year" },
-    yaxis: { title: "Number of Sightings"}
-}
-Plotly.newPlot("plot", data, layout);
+// var layout = {
+//     title: "Sasquatch Sightings by Year",
+//     xaxis: { title: "Year" },
+//     yaxis: { title: "Number of Sightings"}
+// }
+// Plotly.newPlot("plot", data, layout);
