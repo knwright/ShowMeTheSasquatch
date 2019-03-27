1a7d60338901db431a43998a50f36b7238ea9753
Ricky Ravin <ravin013@umn.edu>
2019-03-26 19:44:08 -0500

    first commit

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from csv import DictReader
-from flask import Flask
+from flask import Flask, render_template
+from flask_restplus import Api
 from toolz import compose, pluck, groupby, valmap, first, unique, get, countby
 import datetime as dt
 # from dotenv import find_dotenv,load_dotenv
 import pymongo 
 import pandas as pd
 import os
-
-
-
-
 ################################################################################
 # APP INITIALIZATION
 ################################################################################
@@ -18,11 +15,21 @@ conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 db=client.Bigfootdb
 
-
-# Flask app
+# Restful flask app
 app = Flask(__name__)
+api = Api(app=app)
+
+
+@api.route("/")
+class mainpage():
+    def get(self):
+        return render_template("index.html")
+
 
 # For Heroku deployment
-server = app.server
+server = api.server
 server.secret_key = os.environ.get("SECRET_KEY", "secret")
 
+
+if __name__ == "__main__":
+    api.run(debug=True)
