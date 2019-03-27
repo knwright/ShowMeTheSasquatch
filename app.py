ae71ab98f6806c82d85f44fc01a730523d7c9234
Ricky Ravin <ravin013@umn.edu>
2019-03-26 21:33:15 -0500

    First commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_restplus import Api
+from flask_restplus import Api, Resource
 import pymongo 
 import pandas as pd
 import os
@@ -7,7 +7,7 @@ import os
 # Read the data.
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
-db=client.Bigfootdb
+db=client.bigfootdb
 
 # Restful flask app
 app = Flask(__name__)
@@ -15,24 +15,23 @@ api = Api(app=app)
 
 # Homepage route displaying the story
 @api.route("/")
-class mainpage():
+class mainpage(Resource):
     def get(self):
         return render_template("index.html")
 
 # Route displaying graphs
 @api.route("/visualizations")
-class graphs():
+class graphs(Resource):
     def get(self):
         return render_template("index.html")
 
 # Route displaying data
 @api.route("/data")
-class data():
+class data(Resource):
     def get(self):
-        db.session.query
-        return render_template("index.html")
-
-
+        data = list(db.STATE.find())
+        print(data)
+        # return render_template("index.html", data=data)
 
 if __name__ == "__main__":
-    api.run(debug=True)
+    app.run(debug=True)
