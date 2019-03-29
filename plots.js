2ebb6ea537a1f84f6bf80be0f4c25507f4be6f12
J. Miller <44529625+miler7@users.noreply.github.com>
2019-03-28 19:52:06 -0500

    Connected live data to fake work to complete chart

--- plots.js
+++ plots.js
@@ -1,24 +1,27 @@
+var fulldata;
+
 // Retrieve CSV data
 d3.csv("bfro_reports_geocoded.csv", function(datacsv) {
-  });
-
-
+  fulldata = datacsv;
+  daychart();
+ });
 
 var fakedates = ['2014-04-03', '2015-11-23', '2009-04-22'];
 
-// Create empty array to hold days of week
+var initialdates = [];
 var mydayarray = [];
 
-// Build for loop to convert date format to day of week
-var i;
-for (i = 0; i < fakedates.length; i++) {
-  var parts = fakedates[i].split('-');
-  var mydate = new Date(parts[0], parts[1] - 1, parts[2]); 
-  var mydatestring = mydate.toDateString();
-  var mydatearray = mydatestring.split(' ');
-  var myday = mydatearray[0];
-  mydayarray.push(myday);
-}
+function daychart() {
+  for (i =0; i < fulldata.length; i++) {
+    var activedate = fulldata[i].date;
+    initialdates.push(activedate);
+    var parts = initialdates[i].split('-');
+    var mydate = new Date(parts[0], parts[1] - 1, parts[2]); 
+    var mydatestring = mydate.toDateString();
+    var mydatearray = mydatestring.split(' ');
+    var myday = mydatearray[0];
+    mydayarray.push(myday);
+  }
 
 // Create empty array to hold total sightings for each day of week
 var daytotals = {};
@@ -28,8 +31,8 @@ mydayarray.forEach(function(x) { daytotals[x] = (daytotals[x] || 0)+1; });
 
 // Create the Trace
 var trace1 = {
-  x: ["Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"],
-  y: [daytotals.Mon, daytotals.Tues, daytotals.Wed, daytotals.Thu, daytotals.Fri, daytotals.Sat, daytotals.Sun],
+  x: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  y: [daytotals.Mon, daytotals.Tue, daytotals.Wed, daytotals.Thu, daytotals.Fri, daytotals.Sat, daytotals.Sun],
   type: "bar"
 };
 
@@ -45,3 +48,4 @@ var layout = {
 
 // Plot the chart to a div tag with id "bar-plot"
 Plotly.newPlot("bar-plot", data, layout);
+}
