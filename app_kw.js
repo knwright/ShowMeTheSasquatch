74301ae27d90577e37f65976fede8b8d551f9193
Knwright <43178849+knwright@users.noreply.github.com>
2019-03-28 20:43:39 -0500

    updated apps

--- app_kw.js
+++ app_kw.js
@@ -1,22 +1,27 @@
+d3.csv("bfro_reports_geocoded.csv").then(function(bf_data) {
+    console.log(bf_data);
+
+    buildPlot(bf_data);
+});
+
 // Build line plot with traces
-function buildPlot(sightings) {
-    d3.csv("bfro_reports_geocoded.csv").then(function(bf_data) {
-        console.log(bf_data);
-        // Grab values from the response json object to build the plots
-        var year = bf_data.year;
-        var time_cond = bf_data.time_and_conditions;
-        var county = bf_data.county;
-        var state = bf_data.state;
-        var season = bf_data.season;
-        var loc_details = bf_data.location_details;
+function buildPlot(bf_data) {
+
+        // Grab values from the response object to build the plots
+        var parseDate = d3.timeParse("%Y");
+        var year = bf_data.map(x => parseDate(x.date));
+        var sighting = bf_data.map(x => parseDate(x.date));
+        var county = bf_data.map(x => x.county);
+        var state = bf_data.map(x => x.state);
+        var title = bf_data.map(x => x.title);
+        
 
         // Build initial scatter trace
         var trace1 = {
             type: "scatter",
             mode: "lines",
-            name: sighting_class,
             x: year,
-            y: 5032,
+            y: sighting,
             line: {
                 color: "#277ead"
             }
@@ -26,11 +31,10 @@ function buildPlot(sightings) {
         var trace2 = {
             type: "candlestick",
             x: year,
-            Time_Cond: time_cond,
+            y: sighting,
             County: county,
             State: state,
-            Season: season,
-            Location_Details: loc_details
+            Description: title
         };
 
         var data = [trace1, trace2];
@@ -38,16 +42,15 @@ function buildPlot(sightings) {
         var layout = {
             title: `Sasquatch Sightings by Year`,
             xaxis: {
-                range: [1860,2020],
+                range: [1850,2020],
                 type: "year",
             },
             yaxis: {
-                autorange: true,
+                range: [0,3700],
                 type: "linear"
             }
         };
 
         Plotly.newPlot("plot", data, layout);
 
-    });
-}
+    }
