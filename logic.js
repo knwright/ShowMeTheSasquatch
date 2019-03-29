83569d6d2a8008cf29a94896f7a2c6fd5a242ec2
dkings3117 <37988601+dkings3117@users.noreply.github.com>
2019-03-28 20:49:55 -0500

    Created layers for cluster and heat map

--- logic.js
+++ logic.js
@@ -6,7 +6,7 @@ d3.csv("data/bfro_reports_geocoded.csv", function(data) {
 
 function createMap(data) {
 
-  // Define map
+  // Define variables for our base layers
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -14,9 +14,21 @@ function createMap(data) {
     accessToken: API_KEY
   });
 
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+  
+  // console.log(data[1228]);
+
   // Create a new marker cluster group
   var markers = L.markerClusterGroup();
+  var bigfootHeatArray = [];
 
+  var min_year = 2019;
+  var min_year_index = 10000;
   for (var i = 0; i < data.length; i++) {
     // Loop through the data array, create a new marker,
     // and push it to the markers group.
@@ -32,10 +44,49 @@ function createMap(data) {
         + "<hr>" + data[i].observed;
       markers.addLayer(L.marker([data[i].latitude, data[i].longitude])
         .bindPopup(
-          popupString.substring(0, 2568)  // truncate to prevent popup from flashing on and off
+          popupString.substring(0, 2348)  // truncate to prevent popup from flashing on and off
       ));
+      // push the location to the heat array
+      bigfootHeatArray.push([parseFloat(data[i].latitude), parseFloat(data[i].longitude)]);
+    }
+    // find first year of data
+    if (data[i].date != "")
+    {
+        var year = data[i].date.substring(0,4);
+        year = parseInt(year);
+        // console.log(year);
+        if (year < min_year)
+        {
+          min_year = year;
+          min_year_index = i;
+        }
     }
   }
+  console.log("min_year = " + min_year);
+  console.log("min_year_index = " + min_year_index);
+
+  var bigfootHeat = L.heatLayer(bigfootHeatArray, {
+    radius: 25,       // radius of each "point" of the heatmap, 25 by default
+    blur: 15,         // amount of blur, 15 by default
+    minOpacity: 0.5,  // the minimum opacity the heat will start at
+    maxZoom: 18,      // zoom level where the points reach maximum intensity
+    max: 1.0          // maximum point intensity, 1.0 by default
+  });
+
+  var sightingLayer = L.layerGroup(markers);
+  var heatLayer = L.layerGroup(bigfootHeat);
+
+  // Create a baseMaps object
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  // Create an overlay object
+  var overlayMaps = {
+    "Clusters": markers,
+    "Heat Map": bigfootHeat
+  };
 
   // Create our map, giving it the streetmap and sighting layers to display on load
   var myMap = L.map("map", {
@@ -43,9 +94,13 @@ function createMap(data) {
       45, -93
     ],
     zoom: 4,
-    layers: [streetmap]
+    layers: [streetmap, markers, bigfootHeat]
   });
 
-  myMap.addLayer(markers);
+  // Pass our map layers into our layer control
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+  
 }
-
