9bac90376958227d5b1057b263953a51541f3261
Ricky Ravin <ravin013@umn.edu>
2019-03-30 10:30:45 -0500

    first commit

--- app.py
+++ app.py
@@ -1,41 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_restplus import Api, Resource
 import pymongo
 from flask_pymongo import PyMongo
 import pandas as pd
 import os
 
-# Read the data.
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-db=client.bigfootdb
-
 # Restful flask app
 app = Flask(__name__)
 api = Api(app=app)
+bigfoot = api.namespace ('BigFoot', description = 'BigFoot Sightings')
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/bigfootfb"
-mongo = PyMongo(app)
+# Load the data from mongodb
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.bigfootdb
+collection = db.metadata
+
+print(collection)
 
 # Homepage route displaying the story
-@api.route("/")
+@bigfoot.route("/")
 class mainpage(Resource):
     def get(self):
-        return render_template("index.html")
+        """
+        Displays the story of bigfoot
+        """
+        return ('Hello World')
 
 # Route displaying graphs
-@api.route("/visualizations")
+@bigfoot.route("/visualizations")
 class graphs(Resource):
     def get(self):
+        """
+        Displays the graphs
+        """
         return render_template("index.html")
 
 # Route displaying data
-@api.route("/data")
+@bigfoot.route("/data")
 class data(Resource):
     def get(self):
-        data = mongo.db.listings.find()
-        print(data)
-        # return render_template("index.html", data=data)
+        """
+        Displays the data collection
+        """
+        # results = collection.find()
+        # return results
 
+        df = pd.read_csv("data/bfro_reports_geocoded.csv")
+        df
+      
+# Initialize app
 if __name__ == "__main__":
     app.run(debug=True)
